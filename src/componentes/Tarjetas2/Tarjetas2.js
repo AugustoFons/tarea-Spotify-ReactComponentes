7b3e12cb477f4a3675858eda66561137c70ff4a9
AugustoFons <augustofonsdev@gmail.com>
2022-11-28 15:46:40 -0300

    correcciones

--- src/componentes/Tarjetas2/Tarjetas2.js
+++ src/componentes/Tarjetas2/Tarjetas2.js
@@ -11,21 +11,19 @@ const Tarjetas2 = (props) => {
     };
 
     const [checked, setChecked] = useState(false); 
-    const handleChange = () => { 
-      setChecked(!checked);       
+    const handleChange = e => { 
+        e.preventDefault();
+        setChecked(!checked)
     }
 
     return (
     <figure className="tj2" onMouseEnter={mostrarBoton} onMouseLeave={mostrarBoton} >
-    <div style={{height:"65%", width:"100%",}}>
-        <img style={{width:"100%", borderRadius:"5px",boxShadow: "0px 5px 8px 9px #101010"}} src={props.info.imagen} alt="" />
+    <div style={{minHeight:"65%", width:"100%", border:"black solid 2px"}}>
+        <img style={{width:"100%",minHeight:"100%", borderRadius:"5px",boxShadow: "0px 5px 8px 9px #101010"}} src={props.info.imagen} alt="" />
         <img src={botonPlay} alt="" className={checked ? 'btnPausa2' : isActive ? "btn" : "btn-visible2"  } onClick={handleChange} />
     </div>
     <figcaption>
-        <h4 style={{
-            color: "white",
-            lineHeight:"8px"
-        }}>{props.info.titulo}</h4>
+        <h4 style={{color: "white",}}>{props.info.titulo}</h4>
         <p style={{color: "white"}}>{props.info.descripcion}</p>
     </figcaption>
 
