bca55695140266bb4e11d3af7760402eb9be7120
AugustoFons <augustofonsdev@gmail.com>
2024-03-31 19:48:40 -0300

    redeploy

--- src/componentes/Seccion.js
+++ src/componentes/Seccion.js
@@ -7,28 +7,28 @@ import gh from "./img/gh.png"
 const Seccion = (props) => {
     return (
         <PlayContextProvider>
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems:"flex-start",
-            padding:"20px 20px 20px 50px",
-            rowGap:"30px"
-        }}>
-        <h1 style={{color: "white" }}>Good evening</h1>
-        <ListaTarjetas1 lista={props.lista} />
-        <h2 style={{color: "white", }}>Recently played</h2>
-        <ListaTarjetas2 lista={props.lista} />
-        <h2 style={{color: "white"}}>jump back in</h2>
-        <ListaTarjetas3 lista={props.lista} />
-        <a href="https://github.com/AugustoFons" style={{textDecoration: 'none'}}>
-            <div style={{display:'flex', alignItems: 'center', columnGap: '10px', cursor: 'pointer', flexWrap:'wrap'}}>
-                    <img src={gh} alt="" />
-                    <h3 style={{color: "white"}}>By augustofonsdev@gmail.com</h3>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems:"flex-start",
+                padding:"20px 20px 20px 50px",
+                rowGap:"30px"
+            }}>
+            <h1 style={{color: "white" }}>Good evening</h1>
+            <ListaTarjetas1 lista={props.lista} />
+            <h2 style={{color: "white", }}>Recently played</h2>
+            <ListaTarjetas2 lista={props.lista} />
+            <h2 style={{color: "white"}}>jump back in</h2>
+            <ListaTarjetas3 lista={props.lista} />
+            <a href="https://github.com/AugustoFons" style={{textDecoration: 'none'}}>
+                <div style={{display:'flex', alignItems: 'center', columnGap: '10px', cursor: 'pointer', flexWrap:'wrap'}}>
+                        <img src={gh} alt="" />
+                        <h3 style={{color: "white"}}>By augustofonsdev@gmail.com</h3>
+                </div>
+            </a>
+
+
             </div>
-        </a>
-
-
-        </div>
         </PlayContextProvider>
     )
 }
